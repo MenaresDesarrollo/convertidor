4c437b126989562eedbf939d3f565cba4b916157
Carlos Menares <menaresdesarrollo@gmail.com>
2023-03-06 17:41:23 -0300

    Prueba de convertidor paso 1 aun sin terminar

---
+++ src/app/components/convertidor/convertidor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-convertidor',
+  templateUrl: './convertidor.component.html',
+  styleUrls: ['./convertidor.component.css']
+})
+export class ConvertidorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
